b2da13d266c54d0b5676c78e026db857dce406d2
The-Adam-The <adamwhite930@gmail.com>
2022-03-30 18:40:27 +0100

    created base RPSLS game

---
+++ data.py
@@ -0,0 +1,42 @@
+moves = {
+       "1": ["scissors", {
+        "paper": "You win! Scissors cuts paper!",
+        "lizard": "You win! Scissors decapitates lizard!",
+        "scissors": "It's a draw!",
+        "rock": "You lose! Rock crushes scissors!",
+        "spock": "You lose! Spock smashes scissors!"
+       }
+       ],
+    "2": ["paper", {
+        "rock": "You win! Paper covers rock!",
+        "spock": "You Win! Paper disproves Spock!",
+         "paper": "It's a draw!",
+        "scissors": "You lose! Scissors cuts paper!",
+        "lizard": "You lose! Lizard eats paper!",
+        }
+    ],
+    "3": ["lizard", {
+        "spock": "You win! Lizard poisons Spock!",
+        "paper": "You win! Lizard eats paper!",
+        "lizard": "It's a draw!",
+        "scissors": "You lose! Scissors decapitates lizard!",
+        "rock": "You lose! Rock crushes lizard!",
+        }
+    ],
+    "4": ["rock", {
+        "scissors": "You win! Rock crushes scissors!",
+        "lizard": "You win! Rock crushes lizard!",
+        "rock": "It's a draw!",
+        "spock": "You lose! Spock vaporises rock!",
+        "paper": "You lose! Paper covers rock!",
+        }
+    ],
+    "5": ["spock", {
+        "rock": "You win, Spock vaporises rock!",
+        "scissors": "You win, Spock smashes scissors!",
+        "spock": "It's a draw!",
+        "lizard": "You lose, Lizard poisons Spock!",
+        "paper": "You lose, Paper disproves Spock!",
+    }
+    ]
+}
---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+import art 
+from data import moves
+
+while __name__ == "__main__":
+    player_move_select = input("""Select your move! 
+    1. Scissors 
+    2. Paper 
+    3. Lizard 
+    4. Rock 
+    5. Spock.
+    """)
+    
+    player_move = moves[player_move_select][0]
+    cpu_move = random.choice(list(moves[player_move_select][1].keys()))
+
+    outcome = moves[player_move_select][1][cpu_move]
+ 
+
+    print("\n")
+    print(f"You chose: {player_move}.")
+    print("\n")
+    time.sleep(1)
+    print("Rock, Spock, Scissors, Lizard, Paper!")
+    time.sleep(1)
+    print("Rock, Spock, Scissors, Lizard, Paper!")
+    time.sleep(1)
+    print("Rock, Spock, Scissors, Lizard, Paper!")
+    time.sleep(1)
+    print("\n")
+
+    print(f"Your opponent chose: {cpu_move}")
+    print("\n")
+    print(outcome)
+    print("\n")
+
