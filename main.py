32615ec36f6e15ea82df3186fe2859c2ccb3c63b
The-Adam-The <adamwhite930@gmail.com>
2022-03-31 12:15:10 +0100

    changed terminal window sizes and allowed screen clears

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import random
 import time
+import sys
+import os
 
 from art import art 
 from data import moves
 
+
+sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=50, cols=100))
+
 while __name__ == "__main__":
+
+    os.system('cls||clear')
 
     player_move_select = input("""Select your move! 
     1. Scissors 
@@ -13,24 +20,24 @@ while __name__ == "__main__":
     4. Rock 
     5. Spock.
     """)
+    import os
+    os.system('cls||clear')
     
     player_move = moves[player_move_select][0]
     cpu_move = random.choice(list(moves[player_move_select][1].keys())) 
-
-    print(f"player_move: {player_move}")
-    print(f"cpu_move: {cpu_move}")
-
     outcome = moves[player_move_select][1][cpu_move]
     
 
     
     print(f"You chose: {player_move}.")
+    time.sleep(1)
     print("\n")
-    time.sleep(1)
     print("Rock, Spock, Scissors, Lizard, Paper!")
     time.sleep(1)
+    print("\n")
     print("Rock, Spock, Scissors, Lizard, Paper!")
     time.sleep(1)
+    print("\n")
     print("Rock, Spock, Scissors, Lizard, Paper!")
     time.sleep(1)
     print("\n")
@@ -46,6 +53,10 @@ while __name__ == "__main__":
         print(art[str(player_move)])
     else:
         print(art[str(cpu_move)])
+
+    play_again = input("Play again? y/n")
+    if play_again == "n":
+        quit()
     
     print("\n")
 
